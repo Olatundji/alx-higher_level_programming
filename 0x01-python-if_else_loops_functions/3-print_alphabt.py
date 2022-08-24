f77ef01af5427134884df4dd31bfdccea2cbe8aa
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-08-24 22:39:52 +0100

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
- in 'abcdefghijklmnopqrstuvwxyz':
-
-         if (i != "q" and i != "e"):
-
-                     print("{}".format(i), end="")
+#!/usr/bin/python3
+for i in 'abcdefghijklmnopqrstuvwxyz':
+    if (i != "q" and i != "e"):
+        print("{}".format(i), end="")
