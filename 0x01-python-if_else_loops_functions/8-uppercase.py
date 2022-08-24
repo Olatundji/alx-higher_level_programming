ec5743df73a8dca07b05249eeeead23305e89ca7
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-08-24 22:37:17 +0100

    Update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,13 @@
-se(str):
-
-        for i in range(len(str)):
-
-                    x = ord(str[i])
-
-                            if x >= 97 and x <= 122:
-
-                                            x = x - 32
-
-                                                    print("{}".format(chr(x)), end="")
-
-                                                        print()
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        x = ord(str[i])
+        if x >= 97 and x <= 122:
+            x = x - 32
+        print("{}".format(chr(x)), end="")
+    print()
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
