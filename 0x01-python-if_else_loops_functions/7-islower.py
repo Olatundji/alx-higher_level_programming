06f4f679689234de01b635d02e067c4943602fd5
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-08-24 22:37:47 +0100

    Update 7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,11 +1,7 @@
-slower(c):
-
-        i = ord(c)
-
-            if i >= 97 and i <= 122:
-
-                        return True
-
-                        else:
-
-                                    return False
+#!/usr/bin/python3
+def islower(c):
+    i = ord(c)
+    if i >= 97 and i <= 122:
+        return True
+    else:
+        return False
