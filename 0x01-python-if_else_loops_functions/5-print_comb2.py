df48bf80e77d9658c0f3c312b600caa5c4c7a573
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-08-24 22:38:51 +0100

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,6 @@
- in range(00, 100):
-
-         if i == 99:
-
-                     print("{:02d}".format(i))
-
-                             break
-
-                             print("{:02d}, ".format(i), end="")
+#!/usr/bin/python3
+for i in range(00, 100):
+    if i == 99:
+        print("{:02d}".format(i))
+        break
+    print("{:02d}, ".format(i), end="")
