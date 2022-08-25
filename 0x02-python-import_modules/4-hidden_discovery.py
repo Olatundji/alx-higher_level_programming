4bf156976c1bcb1b71e5bb2cb97ef67e0a4b1a26
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-08-25 19:50:57 +0100

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,6 @@
-f __name__ == "__main__":
-
-        import hidden_4
-
-            for word in dir(hidden_4):
-
-                        if word[1] != '_':
-
-                                        print(word)
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    for word in dir(hidden_4):
+        if word[1] != '_':
+            print(word)
