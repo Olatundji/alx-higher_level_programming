42a979620e04b96bdea427f6c31c6d784f6613ed
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-08-25 19:50:24 +0100

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,7 @@
-f __name__ == "__main__":
-
-        from sys import argv
-
-            sum_var = 0
-
-                for i in range(1, len(argv)):
-
-                            sum_var += int(argv[i])
-
-                                print("{}".format(sum_var))
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    sum_var = 0
+    for i in range(1, len(argv)):
+        sum_var += int(argv[i])
+    print("{}".format(sum_var))
