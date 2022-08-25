60b241524e515135b00d6f8b08509fdfbd4c5244
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-08-25 19:45:59 +0100

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,19 +1,11 @@
-dd.py
-
+#!/usr/bin/python3
+# 0_add.py
 # Yovo Koffi Vianney
 
-
-
 if __name__ == "__main__":
+    """Print the sum of 1 and 2."""
+    from add_0 import add
 
-        """Print the sum of 1 and 2."""
-
-            from add_0 import add
-
-
-
-                a = 1
-
-                    b = 2
-
-                        print("{} + {} = {}".format(a, b, add(a, b)))
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
