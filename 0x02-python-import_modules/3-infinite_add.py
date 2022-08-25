b23a477ca4037b4abe9df43271b1350fa8ddca19
Olatundji <oikpaye@gmail.com>
2022-08-25 11:44:50 -0700

    Programming python

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+f __name__ == "__main__":
+
+        from sys import argv
+
+            sum_var = 0
+
+                for i in range(1, len(argv)):
+
+                            sum_var += int(argv[i])
+
+                                print("{}".format(sum_var))
---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+f __name__ == "__main__":
+
+        import hidden_4
+
+            for word in dir(hidden_4):
+
+                        if word[1] != '_':
+
+                                        print(word)
