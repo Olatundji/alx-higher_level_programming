efd7ecfc5a2386c1e504437cf9221d8acb1b10ff
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-08-31 16:36:31 +0100

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,6 @@
-quare_matrix_simple(matrix=[]):
-
-        squared = []
-
-            for line in matrix:
-
-                        squared.append([c**2 for c in line])
-
-                            return squared
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    squared = []
+    for line in matrix:
+        squared.append([c**2 for c in line])
+    return squared
