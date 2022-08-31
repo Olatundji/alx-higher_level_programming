601aaadb572c29a1e5a5225bb1478707a025338c
Olatundji <oikpaye@gmail.com>
2022-08-31 08:18:14 -0700

    Programming python

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+quare_matrix_simple(matrix=[]):
+
+        squared = []
+
+            for line in matrix:
+
+                        squared.append([c**2 for c in line])
+
+                            return squared
