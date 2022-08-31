007f78691ed14982289ad9212037421f0678d21e
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-08-31 16:44:21 +0100

    Update 7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,4 @@
-
-nary(my_dict, key, value):
-
-        my_dict[key] = value
-
-            return my_dict
+#!/usr/bin/python3
+def update_dictionary(my_dict, key, value):
+    my_dict[key] = value
+    return my_dict
