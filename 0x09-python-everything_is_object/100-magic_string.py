80c6d01e3292e37f062c4eb5d0872c947680b54e
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-09-21 13:46:10 +0100

    Update 100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
-c_string():
-
-        magic_string.n = getattr(magic_string, 'n', 0) + 1
-
-            return ("Holberton, " * (magic_string.n - 1) + "Holberton")
+#!/usr/bin/python3
+def magic_string():
+    magic_string.n = getattr(magic_string, 'n', 0) + 1
+    return ("Holberton, " * (magic_string.n - 1) + "Holberton")
