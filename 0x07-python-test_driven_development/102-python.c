9f95ceaefae30b543daa270acbabb8d885ddf34c
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-09-21 01:12:50 +0100

    Update 102-python.c

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,131 +1,28 @@
-nclude <Python.h>
-
-
-
-void print_python_list(PyObject *p);
-
-void print_python_bytes(PyObject *p);
-
-
+#include "Python.h"
 
 /**
- *
- *  * print_python_list - Prints basic info about Python lists.
- *
- *   * @p: A PyObject list object.
- *
- *    */
-
-void print_python_list(PyObject *p)
-
+ * print_python_string - Prints information about Python strings.
+ * @p: A PyObject string object.
+ */
+void print_python_string(PyObject *p)
 {
+	long int length;
 
-		int size, alloc, i;
+	fflush(stdout);
 
-			const char *type;
+	printf("[.] string object info\n");
+	if (strcmp(p->ob_type->tp_name, "str") != 0)
+	{
+		printf("  [ERROR] Invalid String Object\n");
+		return;
+	}
 
-				PyListObject *list = (PyListObject *)p;
+	length = ((PyASCIIObject *)(p))->length;
 
-					PyVarObject *var = (PyVarObject *)p;
-
-
-
-						size = var->ob_size;
-
-							alloc = list->allocated;
-
-
-
-								printf("[*] Python list info\n");
-
-									printf("[*] Size of the Python List = %d\n", size);
-
-										printf("[*] Allocated = %d\n", alloc);
-
-
-
-											for (i = 0; i < size; i++)
-
-													{
-
-																type = list->ob_item[i]->ob_type->tp_name;
-
-																		printf("Element %d: %s\n", i, type);
-
-																				if (strcmp(type, "bytes") == 0)
-
-																								print_python_bytes(list->ob_item[i]);
-
-																					}
-
-}
-
-
-
-/**
- *
- *  * print_python_bytes - Prints basic info about Python byte objects.
- *
- *   * @p: A PyObject byte object.
- *
- *    */
-
-void print_python_bytes(PyObject *p)
-
-{
-
-		unsigned char i, size;
-
-			PyBytesObject *bytes = (PyBytesObject *)p;
-
-
-
-				printf("[.] bytes object info\n");
-
-					if (strcmp(p->ob_type->tp_name, "bytes") != 0)
-
-							{
-
-										printf("  [ERROR] Invalid Bytes Object\n");
-
-												return;
-
-													}
-
-
-
-						printf("  size: %ld\n", ((PyVarObject *)p)->ob_size);
-
-							printf("  trying string: %s\n", bytes->ob_sval);
-
-
-
-								if (((PyVarObject *)p)->ob_size > 10)
-
-											size = 10;
-
-									else
-
-												size = ((PyVarObject *)p)->ob_size + 1;
-
-
-
-										printf("  first %d bytes: ", size);
-
-											for (i = 0; i < size; i++)
-
-													{
-
-																printf("%02hhx", bytes->ob_sval[i]);
-
-																		if (i == (size - 1))
-
-																						printf("\n");
-
-																				else
-
-																								printf(" ");
-
-																					}
-
+	if (PyUnicode_IS_COMPACT_ASCII(p))
+		printf("  type: compact ascii\n");
+	else
+		printf("  type: compact unicode object\n");
+	printf("  length: %ld\n", length);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
 }
