5975387c411ed68bb64fa7a20966284abb8c2f0a
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-09-21 01:11:20 +0100

    Update 102-compile.fish

--- 0x07-python-test_driven_development/102-compile.fish
+++ 0x07-python-test_driven_development/102-compile.fish
@@ -1,14 +1,7 @@
-#!/usr/bin/python3
-# 101-lazy_matrix_mul.py
-"""Defines a matrix multiplication function using NumPy."""
-import numpy as np
-
-
-def lazy_matrix_mul(m_a, m_b):
-    """Return the multiplication of two matrices.
-    Args:
-        m_a (list of lists of ints/floats): The first matrix.
-        m_b (list of lists of ints/floats): The second matrix.
-    """
-
-    return (np.matmul(m_a, m_b))
+#!/usr/bin/fish
+# set PY_BIN_PATH /usr/bin/python3.4
+# set PY_INC_PATH /usr/include/python3.4
+set PY_BIN_PATH ~/apps/Python-3.4.10/python
+set PY_INC_PATH ~/apps/Python-3.4.10/Include/:"$HOME/apps/Python-3.4.10/"
+gcc -Wall -Werror -Wextra -pedantic -std=c99 -shared -Wl,-soname,libPython.so -o libPython.so -fPIC -I$PY_INC_PATH 102-python.c
+# [ $status -eq 0 ] && $PY_BIN_PATH ./102-python.py
