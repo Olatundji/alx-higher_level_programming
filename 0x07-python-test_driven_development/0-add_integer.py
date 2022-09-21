c518b2f80cda163de2c1b3aaf79d01c21cdab897
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-09-21 01:07:31 +0100

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,131 +1,16 @@
-nclude <Python.h>
-
-
-
-void print_python_list(PyObject *p);
-
-void print_python_bytes(PyObject *p);
-
-
-
-/**
-
- * print_python_list - Prints basic info about Python lists.
-
-  * @p: A PyObject list object.
-
-   */
-
-   void print_python_list(PyObject *p)
-
-   {
-
-            int size, alloc, i;
-
-                const char *type;
-
-                    PyListObject *list = (PyListObject *)p;
-
-                        PyVarObject *var = (PyVarObject *)p;
-
-
-
-                            size = var->ob_size;
-
-                                alloc = list->allocated;
-
-
-
-                                    printf("[*] Python list info\n");
-
-                                        printf("[*] Size of the Python List = %d\n", size);
-
-                                            printf("[*] Allocated = %d\n", alloc);
-
-
-
-                                                for (i = 0; i < size; i++)
-
-                                                    {
-
-                                                                type = list->ob_item[i]->ob_type->tp_name;
-
-                                                                        printf("Element %d: %s\n", i, type);
-
-                                                                                if (strcmp(type, "bytes") == 0)
-
-                                                                                            print_python_bytes(list->ob_item[i]);
-
-                                                                                                }
-
-                                                    }
-
-
-
-   /**
-
-    * print_python_bytes - Prints basic info about Python byte objects.
-
-     * @p: A PyObject byte object.
-
-      */
-
-      void print_python_bytes(PyObject *p)
-
-      {
-
-                unsigned char i, size;
-
-                    PyBytesObject *bytes = (PyBytesObject *)p;
-
-
-
-                        printf("[.] bytes object info\n");
-
-                            if (strcmp(p->ob_type->tp_name, "bytes") != 0)
-
-                                {
-
-                                            printf("  [ERROR] Invalid Bytes Object\n");
-
-                                                    return;
-
-                                                        }
-
-
-
-                                    printf("  size: %ld\n", ((PyVarObject *)p)->ob_size);
-
-                                        printf("  trying string: %s\n", bytes->ob_sval);
-
-
-
-                                            if (((PyVarObject *)p)->ob_size > 10)
-
-                                                    size = 10;
-
-                                                        else
-
-                                                                size = ((PyVarObject *)p)->ob_size + 1;
-
-
-
-                                                                    printf("  first %d bytes: ", size);
-
-                                                                        for (i = 0; i < size; i++)
-
-                                                                            {
-
-                                                                                        printf("%02hhx", bytes->ob_sval[i]);
-
-                                                                                                if (i == (size - 1))
-
-                                                                                                            printf("\n");
-
-                                                                                                                    else
-
-                                                                                                                                printf(" ");
-
-                                                                                                                                    }
-
-                                                                            }
+#!/usr/bin/python3
+# 0-add_integer.py
+"""Defines an integer addition function."""
+
+
+def add_integer(a, b=98):
+    """Return the integer addition of a and b.
+    Float arguments are typecasted to ints before addition is performed.
+    Raises:
+        TypeError: If either of a or b is a non-integer and non-float.
+    """
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
