3988e03d34e40b098e357113fdac035bce810a55
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-09-21 01:14:26 +0100

    Update 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,131 +1,18 @@
-nclude <Python.h>
-
-
-
-void print_python_list(PyObject *p);
-
-void print_python_bytes(PyObject *p);
-
-
-
-/**
-
- * print_python_list - Prints basic info about Python lists.
-
-  * @p: A PyObject list object.
-
-   */
-
-   void print_python_list(PyObject *p)
-
-   {
-
-            int size, alloc, i;
-
-                const char *type;
-
-                    PyListObject *list = (PyListObject *)p;
-
-                        PyVarObject *var = (PyVarObject *)p;
-
-
-
-                            size = var->ob_size;
-
-                                alloc = list->allocated;
-
-
-
-                                    printf("[*] Python list info\n");
-
-                                        printf("[*] Size of the Python List = %d\n", size);
-
-                                            printf("[*] Allocated = %d\n", alloc);
-
-
-
-                                                for (i = 0; i < size; i++)
-
-                                                    {
-
-                                                                type = list->ob_item[i]->ob_type->tp_name;
-
-                                                                        printf("Element %d: %s\n", i, type);
-
-                                                                                if (strcmp(type, "bytes") == 0)
-
-                                                                                            print_python_bytes(list->ob_item[i]);
-
-                                                                                                }
-
-                                                    }
-
-
-
-   /**
-
-    * print_python_bytes - Prints basic info about Python byte objects.
-
-     * @p: A PyObject byte object.
-
-      */
-
-      void print_python_bytes(PyObject *p)
-
-      {
-
-                unsigned char i, size;
-
-                    PyBytesObject *bytes = (PyBytesObject *)p;
-
-
-
-                        printf("[.] bytes object info\n");
-
-                            if (strcmp(p->ob_type->tp_name, "bytes") != 0)
-
-                                {
-
-                                            printf("  [ERROR] Invalid Bytes Object\n");
-
-                                                    return;
-
-                                                        }
-
-
-
-                                    printf("  size: %ld\n", ((PyVarObject *)p)->ob_size);
-
-                                        printf("  trying string: %s\n", bytes->ob_sval);
-
-
-
-                                            if (((PyVarObject *)p)->ob_size > 10)
-
-                                                    size = 10;
-
-                                                        else
-
-                                                                size = ((PyVarObject *)p)->ob_size + 1;
-
-
-
-                                                                    printf("  first %d bytes: ", size);
-
-                                                                        for (i = 0; i < size; i++)
-
-                                                                            {
-
-                                                                                        printf("%02hhx", bytes->ob_sval[i]);
-
-                                                                                                if (i == (size - 1))
-
-                                                                                                            printf("\n");
-
-                                                                                                                    else
-
-                                                                                                                                printf(" ");
-
-                                                                                                                                    }
-
-                                                                            }
+#!/usr/bin/python3
+# 3-say_my_name.py
+"""Defines a name-printing function."""
+
+
+def say_my_name(first_name, last_name=""):
+    """Print a name.
+    Args:
+        first_name (str): The first name to print.
+        last_name (str): The last name to print.
+    Raises:
+        TypeError: If either of first_name or last_name are not strings.
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
