324dae5b49fd364486448471b76ed937616af10f
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-09-29 01:39:55 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,13 +1,37 @@
-d_file.py
+#!/usr/bin/python3
+'''A script that stores arguments to a JSON file.
+'''
+import sys
+import os
+from importlib import import_module as using
 
-"""Defines a text file-reading function."""
+
+save_to_json_file, load_from_json_file = (
+    using('5-save_to_json_file').save_to_json_file,
+    using('6-load_from_json_file').load_from_json_file
+)
+'''The functions for saving and loading lists.
+'''
+args_list = []
+'''The list of arguments.
+'''
+args_list_file_name = 'add_item.json'
+'''The file name of the file containing the list of arguments.
+'''
 
 
+def run():
+    '''Runs the routines of this script.
+    '''
+    if not os.path.exists(args_list_file_name):
+        with open(args_list_file_name, mode='w', encoding='utf-8') as file:
+            file.write('[]')
+    json_list = load_from_json_file(args_list_file_name)
+    if (type(json_list) is list) and all(type(s) is str for s in json_list):
+        args_list.extend(json_list)
+    args_list.extend(sys.argv[1:])
+    save_to_json_file(args_list, args_list_file_name)
 
 
-
-def read_file(filename=""):
-
-        """Print the contents of a UTF8 text file to stdout."""
-
-            with open(filename, encoding="utf-8") a
+if __name__ == '__main__':
+    run()
