fe070288f8545a61f7b9de174483390ccb967f2f
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-09-29 02:01:32 +0100

    Update 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python3
-'''A module containing IO functions.
+'''A module for managing students.
 '''
 
 
-def write_file(filename="", text=""):
-    '''Writes a UTF-8 encoded text to a file.
-    Args:
-        filename (str): The name of the file to write to.
-        text (str): The content to store in the file.
-    Returns:
-        int: The number of characters written.
+class Student:
+    '''Represents a student.
     '''
-    n = 0
-    with open(filename, mode='w', encoding='utf-8') as file:
-        n = file.write(text)
-    return n
+    def __init__(self, first_name, last_name, age):
+        '''Initializes this student with the given first name,
+        last name, and age.
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''Retrieves a dictionary of this student's attributes.
+        Args:
+            attrs (list): A list of attributes that can be retrieved.
+        Returns:
+            dict: A dictionary of this student's attributes.
+        '''
+        if '__dict__' in dir(self):
+            res = dict()
+            can_filter = False
+            if (type(attrs) is list) and all(type(s) is str for s in attrs):
+                can_filter = True
+            for key in self.__dict__.keys():
+                if (not can_filter) or (can_filter and key in attrs):
+                    res[key] = self.__dict__[key]
+            return res
