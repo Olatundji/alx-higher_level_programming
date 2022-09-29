09f01779c4bd1ed61beb327f6be0d807afcfc447
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-09-29 01:40:36 +0100

    Update 6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,13 +1,19 @@
-d_file.py
-
-"""Defines a text file-reading function."""
-
-
-
+#!/usr/bin/python3
+'''A module containing IO functions.
+'''
+from json import JSONDecoder
 
 
-def read_file(filename=""):
-
-        """Print the contents of a UTF8 text file to stdout."""
-
-            with open(filename, encoding="utf-8") a
+def load_from_json_file(filename):
+    '''Creates an object from its JSON representation stored in a file.
+    Args:
+        filename (str): The name of the file containing the JSON string.
+    Returns:
+        any: An object corresponding to the JSON string in the file,
+        otherwise an exception is thrown.
+    '''
+    lines = []
+    with open(filename, encoding='utf-8') as file:
+        for line in file.readlines():
+            lines.append(line)
+    return JSONDecoder().decode(''.join(lines))
