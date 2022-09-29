40135cb151a9e9e330b70813bdfe07ab1286ff88
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-09-29 01:45:02 +0100

    Update 3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,13 +1,15 @@
-d_file.py
-
-"""Defines a text file-reading function."""
-
-
-
+#!/usr/bin/python3
+'''A module containing IO functions.
+'''
+from json import JSONEncoder
 
 
-def read_file(filename=""):
-
-        """Print the contents of a UTF8 text file to stdout."""
-
-            with open(filename, encoding="utf-8") a
+def to_json_string(my_obj):
+    '''Creates the JSON representation of an object.
+    Args:
+        my_obj (any): An object to convert to JSON.
+    Returns:
+        str: A JSON representation of the object if possible,
+        otherwise an exception is thrown.
+    '''
+    return JSONEncoder().encode(my_obj)
