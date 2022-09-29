4404151c757226e98956e738f8c900ccba6f75ce
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-09-29 01:44:16 +0100

    Update 4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,13 +1,15 @@
-d_file.py
-
-"""Defines a text file-reading function."""
-
-
-
+#!/usr/bin/python3
+'''A module containing IO functions.
+'''
+from json import JSONDecoder
 
 
-def read_file(filename=""):
-
-        """Print the contents of a UTF8 text file to stdout."""
-
-            with open(filename, encoding="utf-8") a
+def from_json_string(my_str):
+    '''Creates an object from its JSON representation.
+    Args:
+        my_str (str): A JSON representation of an object.
+    Returns:
+        any: An object corresponding to the given JSON string,
+        otherwise an exception is thrown.
+    '''
+    return JSONDecoder().decode(my_str)
